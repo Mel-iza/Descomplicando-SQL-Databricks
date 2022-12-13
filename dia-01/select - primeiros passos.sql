6789c65c83a2aaecba1e4bcf10c23e9fd0bec213
Mel-iza <Mel-iza>
2022-12-13 23:40:55 +0000

    first steps

---
+++ dia-01/select - primeiros passos.sql
@@ -0,0 +1,19 @@
+-- Databricks notebook source
+SELECT 'Hello World!'
+
+-- COMMAND ----------
+
+SELECT 1 + 10 * 2 
+
+-- COMMAND ----------
+
+SELECT 10-1000
+
+
+-- COMMAND ----------
+
+SELECT ABS(10-1000)
+
+-- COMMAND ----------
+
+
